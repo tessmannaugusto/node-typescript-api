36ef6603bef3759436d928286a310393de0eba69
Yunes <augusto.yunes@accenture.com>
2023-02-09 10:07:06 -0300

    chore: grouping forecast by hour

--- src/services/forecast.ts
+++ src/services/forecast.ts
@@ -1,4 +1,5 @@
 import { ForecastPoint, StormGlass } from "@src/clients/stormGlass";
+import { time } from "console";
 
 export enum BeachPosition {
   S = 'S',
@@ -15,12 +16,17 @@ export interface Beach {
   user: string;
 }
 
+export interface TimeForecast {
+  time: string;
+  forecast: BeachForecast[];
+}
+
 export interface BeachForecast extends Omit<Beach, 'user'>, ForecastPoint {}
 
 export class Forecast {
   constructor(protected stormGlass = new StormGlass()) {}
 
-  public async processForecastForBeaches(beaches: Beach[]): Promise<BeachForecast[]> {
+  public async processForecastForBeaches(beaches: Beach[]): Promise<TimeForecast[]> {
     const pointsWithCorrectSources: BeachForecast[] = [];
     for(const beach of beaches){
       const points = await this.stormGlass.fetchPoints(beach.lat, beach.lng);
@@ -36,6 +42,22 @@ export class Forecast {
       }));
       pointsWithCorrectSources.push(...enrichedBeachData)
     }
-    return pointsWithCorrectSources;
+    return this.mapForecastByTime(pointsWithCorrectSources);
+  }
+
+  private mapForecastByTime(forecast: BeachForecast[]): TimeForecast[] {
+    const forecastByTime: TimeForecast[] = [];
+    for(const point of forecast) {
+      const timePoint = forecastByTime.find((f) => f.time === point.time);
+      if(timePoint) {
+        timePoint.forecast.push(point);
+      } else {
+        forecastByTime.push({
+          time: point.time,
+          forecast: [point],
+        })
+      }
+    }
+    return forecastByTime;
   }
 }
